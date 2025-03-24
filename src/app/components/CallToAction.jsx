b790fa4dd7391e0e7c0e5519bd492a6f43202f2b
smshuvo100 <smshuvo100@gmail.com>
2025-03-24 13:23:37 +0600

    single page done

---
+++ src/app/components/CallToAction.jsx
@@ -0,0 +1,18 @@
+export default function CallToAction() {
+  return (
+    <div className="flex-box">
+      <div className="text">
+        <h2>Want to learn more about JavaScript?</h2>
+        <p>Checkout these resources with 100 JavaScript Projects</p>
+        <button className="sm-btn">
+          <a href="https://www.100jsprojects.com" target="_blank" rel="noopener noreferrer">
+            100 JavaScript Projects
+          </a>
+        </button>
+      </div>
+      <div className="image">
+        <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+      </div>
+    </div>
+  );
+}
---
+++ src/app/post/[slug]/page.jsx
@@ -0,0 +1,47 @@
+import CallToAction from "../../components/CallToAction";
+
+import Link from "next/link";
+export default async function PostPage({ params }) {
+  let post = null;
+  try {
+    const result = await fetch(process.env.URL + "/api/post/get", {
+      method: "POST",
+      body: JSON.stringify({ slug: params.slug }),
+      cache: "no-store"
+    });
+    const data = await result.json();
+    post = data.posts[0];
+  } catch (error) {
+    post = { title: "Failed to load post" };
+  }
+  if (!post || !post.title === "Failed to load post") {
+    return (
+      <div className="not-found">
+        <h2>Post not found</h2>
+      </div>
+    );
+  }
+  return (
+    <div className="single-post">
+      <div className="container">
+        <h1>{post && post.title}</h1>
+        <Link href={`/search?category=${post && post.category}`} className="cat-btn">
+          <button>{post && post.category}</button>
+        </Link>
+        <img src={post && post.image} alt={post && post.title} />
+
+        <div className="post-info">
+          <div className="post-meta">
+            <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+            <span> {post && (post?.content?.length / 1000).toFixed(0)} mins read</span>
+          </div>
+          <div className="post-content" dangerouslySetInnerHTML={{ __html: post?.content }}></div>
+        </div>
+
+        <div className="call-to-action">
+          <CallToAction />
+        </div>
+      </div>
+    </div>
+  );
+}
